a3f75b48e6bc109080a2bc53b9cdb6a96452ccc0
Alex Bennett <kestralttr@gmail.com>
2017-06-19 20:31:57 -0700

    adds styling and content to the main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,6 +23,17 @@ class Main extends React.Component {
             </div>
           </div>
         </div>
+        <div id="main-info-container">
+          <h1>Santorini</h1>
+          <div id="main-info">
+            <p>Santorini holds a global standard as one of the most unique and beautiful holiday locations in the world.  From the breathtaking views to the heavenly amenities provided in the luxury resorts scattered across the clif faces, Santorini is the jewel that everyone deserves to experience.</p><br></br>
+            <p>The most enticing prospect of this otherworldly locale is the fact that Santorini offers something for everyone.  Family vacations, honeymoons, retreats, or just some badly-needed personal time; no matter what the occasion, you will find exactly what you desire here on the edge of the Mediterrean.</p><br></br>
+            <p>Feel free to explore all that the wonderful island of Santorini has to offer, and don't hesitate to contact us with any questions.</p><br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,8 +16,8 @@ class Nav extends React.Component {
           <div id="home-link"><Link to="/">Home</Link></div>
           <div className="nav-link"><Link to="/beaches">Beaches</Link></div>
           <div className="nav-link"><Link to="/fooddrink">Food & Drink</Link></div>
-          <div className="nav-link"><Link to="/reservations">Reservations</Link></div>
-          <div className="nav-link"><Link to="/contactus">Contact Us</Link></div>
+          <div className="nav-link"><Link to="/reservations">Travel</Link></div>
+          <div className="nav-link"><Link to="/contact">Contact</Link></div>
         </div>
       </div>
     );
